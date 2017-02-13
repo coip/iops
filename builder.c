dd4772529c29cd731d9a79f43eb0d481215ed09f
coip <november@txstate.edu>
2017-02-13 13:13:54 -0600

    refactoring structure started. handling building matrix better now. added debug flag for prints

--- builder.c
+++ builder.c
@@ -11,7 +11,8 @@
 
 const char delimiter = ',';
 const char EoV = '}';
-
+int debug;
+ 
 struct Vector {
 	int n; 	//# of columns/width
 	int *v; //a malloc(n*sizeof(int))'d int pointer to the vector
@@ -25,35 +26,51 @@ typedef struct {
 
 Matrix buildMatrix(char * buf){
 	
+	//#INIT MATRIX:
+	Matrix matrix;
+	matrix.m = 0;
+	matrix.n = 0; 
+	
+	FILE *mfile;
+	mfile = fopen(buf, "r");
+	if(mfile == NULL){
+		printf("File doesn't exist: %s", buf);
+		fclose(mfile);
+		return matrix;
+	}
+
+//static allocation: eww. todo
+	char storage[100];
+	fgets(storage, 100, (FILE *)mfile);
+	char * buffer = storage;
+
 	//#INIT VECTOR:
 	struct Vector row;
 	row.n = 0;
 	row.v = malloc(sizeof(int));
 
-	//#INIT MATRIX:
-	Matrix matrix;
-	matrix.m = 0;
-	matrix.n = 0; 
 
 	//INIT CSTR
 	char * token = malloc(1);
 	int counter = 0;
 
-	while( *++buf ){
-		if(*buf == delimiter || *buf == EoV){
+	while( *(++buffer) ){//preincrement due to starting char will be opening character following matrix convention.
+		if(*buffer == delimiter || *buffer == EoV){
 			*(row.v+(row.n)) = atoi(token);
 			row.n++;
+if(debug)
 			printf("cstring rep. of #: %s. int %d added. size of row: %d\n\n", token, *(row.v+row.n-1), row.n);
 			row.v = realloc(row.v, ((row.n+1) * 8)); 
 			free(token);
 			counter = 0;
 			token = malloc(1);
 		}
-		if(*buf == EoV)
+		if(*buffer == EoV)
 			break;
-		if((48 <= *buf) && (*buf <= 57)){
-				printf("char: \'%c\', int: %d, addr: %p\n", *buf, *buf, buf);
-			*(token+counter) = *buf;
+		if((48 <= *buffer) && (*buffer <= 57)){
+if(debug)
+			printf("char: \'%c\', int: %d, addr: %p\n", *buffer, *buffer, buffer);
+			*(token+counter) = *buffer;
 			token = realloc(token, (counter+1));
 			*(token+counter+1) = '\0';
 			counter++;
@@ -61,36 +78,26 @@ Matrix buildMatrix(char * buf){
 		
 	}
 	printf("row.n: %d\n", row.n);
-	for(int i = 0; i < row.n; i++)
+int i;
+	for(i = 0; i < row.n; i++)
 		printf("row[%d]: %d\n", i, row.v[i]);
+	fclose(mfile);
 	return matrix;
 
 }
 
 
 int main(int argc, char* argv[]){
-
+debug = 0;
+if(argc == 3) 
+debug = 1;
 
 	printf("building matrix in %s", argv[1]);
-	
 
-	FILE *matrix;
+	buildMatrix(argv[1]);
+//only receiving one line from input currently.
+//refactor to call new helper function buildVector() in buildMatrix(string file). move all file io inside build matrix master call.
 
-	matrix = fopen(argv[1], "r");
-	if(matrix == NULL){
-		printf("File doesn't exist: %s", argv[1]);
-		fclose(matrix);
-		return -1;
-	}
-
-	char buffer[100];
-	fgets(buffer, 100, (FILE *)matrix);
-
-	printf("\nsizeof(buffer): %lu, contents: %s", sizeof(buffer), buffer);
-	
-	buildMatrix(buffer);
-
-	fclose(matrix);
 
 	return 0;
 
