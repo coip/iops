0a6624a7121281768c596a8290b6df8f1f51e621
coip <november@txstate.edu>
2017-02-16 15:36:11 -0600

    polished a bit.

--- builder.c
+++ builder.c
@@ -7,173 +7,172 @@
  * */
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 
 const char delimiter = ',';
 const char EoV = '}';
 
 int d1, d2, d3;
 
-struct Vector {
-	int n;			//# of columns/width
-	int *v;			//a malloc(n*sizeof(int))'d              int pointer to the vector
+struct Vector
+{
+  int n;			//# of columns/width todo kind of redundant. #onthetable
+  int *v;			//a malloc(n*sizeof(int))'d              int pointer to the vector
 };
 
-typedef struct {
-	int m;			//# of rows/height:
-	struct Vector *v;	//a malloc(m*sizeof(struct Vector))'d vector pointer to a vector
+typedef struct
+{
+  int m;			//# of rows/height:
+  struct Vector *v;		//a malloc(m*sizeof(struct Vector))'d vector pointer to a vector
 } Matrix;
-/*
 
-Matrix m;
-[[1,2,3,4,5],
- [6,7,8,9,0],
- [1,3,5,7,9],
- [2,4,6,8,0]]
+void setDebug (char *);
+void printMatrix (Matrix);
 
-m.m == 4;
-m.n == 5;
-m.v == memoryaddr;
-
-*(m)  => struct Vector;
-**(m) => int;
-
-*(m.v)  			== [ 1, 2, 3, 4, 5];
-**(m.v) 			== 1;
-**(m.v+4)			== 2;
-
-*(m.v+sizeof(struct Vector)) 	== [ 6, 7, 8, 9, 0]
-
-*/
-
-void printMatrix(Matrix m);
-struct Vector buildVector(char *buffer)
+struct Vector
+buildVector (char *buffer)
 {				//this takes in input after readline from file.
 
-	//#INIT VECTOR:
-	struct Vector row;
-	row.n = 0;
-	row.v = malloc(sizeof(int));
-	//INIT CSTR
-	char * token = malloc(1);
-	int counter = 0;
-if(d1)
-	printf("building vector from buffer: %s\n", buffer);
+  //#INIT VECTOR:
+  struct Vector row;
+  row.n = 0;
+  row.v = malloc (sizeof (int));
+  //INIT CSTR
+  char *token = malloc (1);	//todo memory could likely be much less granular.
+  int counter = 0;
+  if (d1)
+    printf ("building vector from buffer: %s\n", buffer);
 
-	while (*(++buffer)) {	//preincrement due to starting char will be opening character following matrix convention.
-		if (*buffer == delimiter || *buffer == EoV) {
-			*(row.v + (row.n)) = atoi(token);
-			row.n++;
-			if (d2)
-				printf("cstring rep. of #: %s. int %d added. size of row: %d\nsizeof(row.v) = %lu\n", token, *(row.v + row.n - 1), row.n, sizeof(row.v));
-			row.v = realloc(row.v, ((row.n + 1) * 8));
-			free(token);
-			counter = 0;
-			token = malloc(1);
-		}
-		if (*buffer == EoV)
-			break;
-		if ((48 <= *buffer) && (*buffer <= 57)) {
-			if (d2)
-				printf("char: \'%c\', addr: %p\n", *buffer, buffer);
-			*(token + counter) = *buffer;
-			token = realloc(token, (counter + 1));
-			*(token + counter + 1) = '\0';
-			counter++;
-		}
-
+  while (*(++buffer))
+    {				//preincrement due to starting char will be opening character following matrix convention.
+      if (*buffer == delimiter || *buffer == EoV)
+	{
+	  *(row.v + (row.n)) = atoi (token);
+	  row.n++;
+	  if (d2)
+	    printf
+	      ("cstring rep. of #: %s. int %d added.",
+	       token, *(row.v + row.n - 1));
+	  row.v = realloc (row.v, ((row.n + 1) * 8));
+	  free (token);
+	  counter = 0;
+	  token = malloc (1);
+	}
+      if (*buffer == EoV)
+	break;
+      if ((48 <= *buffer) && (*buffer <= 57))
+	{
+	  if (d2)
+	    printf ("char: \'%c\', addr: %p\n", *buffer, buffer);
+	  *(token + counter) = *buffer;
+	  token = realloc (token, (counter + 1));
+	  *(token + counter + 1) = '\0';
+	  counter++;
 	}
 
-	return row;
+    }
+
+  if (d1)
+    {
+      int i;
+      printf ("printing vector contents for row %d:\n", row.n);
+      printf ("\n[ ");
+      for (i = 0; i < row.n - 1; i++)
+	printf ("%d, ", *(row.v + i));
+      printf ("%d ]\n", *(row.v + row.n - 1));
+    }
+
+  return row;
 }
 
-Matrix buildMatrix(char *buf)
-{	
-	//this takes in file name as cstring
-
-	//#INIT MATRIX:
-	Matrix matrix;
-	matrix.m = 0;
-	matrix.v = malloc(sizeof(struct Vector));
-
-	FILE *mfile;
-	mfile = fopen(buf, "r");
-	if (mfile == NULL) {
-		printf("File doesn't exist: %s\n", buf);
-		return matrix;
-	}
-//static allocation: eww. todo
-	char * storage = malloc(100);//todo: make dynamic read in if possible? maybe pass in buffer size, run tests, derive best?
-
-	while (fgets(storage, 100, (FILE *) mfile)) {
-		//INIT TEMP VECTOR
-		struct Vector r;
-		r = buildVector(storage);
-		*( matrix.v + matrix.m ) = r;
-		if(d1){
-			int i;
-			printf("printing vector contents for row %d:\n", r.n);
-			printf("\n[ ");
-			for (i = 0; i < r.n-1; i++)
-				printf("%d, ", *(r.v+i));
-			printf("%d ]\n", *(r.v+r.n-1));
-		}
-		if(d1){
-			int i;
-			printf("printing vector contents for m.%d:\n", matrix.m);
-			printf("theres %d elements\n", (*(matrix.v)).n);
-			for (i = 0; i < (*(matrix.v)).n - 1 ; i++)
-				printf("%d, ", *((*(matrix.v+matrix.m)).v+i));
-			printf("%d ]\n", *((*(matrix.v+matrix.m)).v+(*(matrix.v+matrix.m)).n-1));
-		}
-if(d2)
-		printf("sizeof(struct Vector *) == %lu;\nsizeof(struct Vector) == %lu;\nr.n = %d;\nsizeof(int) == %lu\n addr: %p\n", 
-				sizeof(struct Vector *), 
-				sizeof(struct Vector), 
-				r.n, 
-				sizeof(int),
-				( matrix.v + (matrix.m * sizeof(struct Vector *))));
-if(d1)
-		printf("total mem for Vector %d: %lu\n\n", matrix.m, (sizeof(struct Vector *) + sizeof(struct Vector) + (r.n * sizeof(int))));
-		matrix.m++;
-		matrix.v = realloc(matrix.v, ((matrix.m + 1) * sizeof(struct Vector)));
-	}
-	fclose(mfile);
-	return matrix;
-
-}
-
-void printMatrix(Matrix m){
-
-	int i, j;
-printf("[[");
-	for(j = 0; j < m.m; j++){
-		for(i = 0; i < (*(m.v)).n -1; i++)
-			printf(" %d, ", *((*(m.v+j)).v+i));
-		printf("%d ]\n [", *((*(m.v+j)).v+(*(m.v+j)).n-1));
-	}
-	printf("end]\n");
-
-}
-
-int main(int argc, char *argv[])
+Matrix
+buildMatrix (char *buf)
 {
-	d1,d2 = 0;
-if(argc == 3)
-	switch(atoi(argv[2])){
-	case 2: d2 = 1;	
-	case 1: d1 = 1;
-	default: break;}
+  //this takes in file name as cstring
+  if (d1)
+    printf ("building matrix in file %s", buf);
+  //#INIT MATRIX:
+  Matrix matrix;
+  matrix.m = 0;
+  matrix.v = malloc (sizeof (struct Vector));
 
-	printf("building matrix in %s\n", argv[1]);
+  FILE *mfile;
+  mfile = fopen (buf, "r");
+  if (mfile == NULL)
+    {
+      printf ("File doesn't exist: %s\n", buf);
+      return matrix;
+    }
+//static allocation: eww. todo
+  char *storage = malloc (100);	//todo: make dynamic read in if possible? maybe pass in buffer size, run tests, derive best?
 
-	Matrix m = buildMatrix(argv[1]);
-	printf("m.m == %d;\n", m.m);
+  while (fgets (storage, 100, (FILE *) mfile))
+    {
+      //INIT TEMP VECTOR
+      struct Vector r;
+      r = buildVector (storage);
+      *(matrix.v + matrix.m) = r;
+      if (d1)
+	{
+	  printMatrix (matrix);
+	}
+      if (d2)
+	printf
+	  ("sizeof(struct Vector *) == %lu;\nsizeof(struct Vector) == %lu;\nr.n = %d;\nsizeof(int) == %lu\n addr: %p\n",
+	   sizeof (struct Vector *), sizeof (struct Vector), r.n,
+	   sizeof (int), (matrix.v + (matrix.m * sizeof (struct Vector *))));
+      matrix.m++;
+      matrix.v =
+	realloc (matrix.v, ((matrix.m + 1) * sizeof (struct Vector)));
+    }
+  fclose (mfile);
+  return matrix;
 
-	printMatrix(m);
-//only receiving one line from input currently.
-//refactor to call new helper function buildVector() in buildMatrix(string file). move all file io inside build matrix master call.
+}
 
-	return 0;
+void
+printMatrix (Matrix m)
+{
+
+  int i, j;
+  printf ("[[");
+  for (j = 0; j < m.m; j++)
+    {
+      for (i = 0; i < (*(m.v)).n - 1; i++)
+	printf (" %d, ", *((*(m.v + j)).v + i));
+      printf ("%d ]\n [", *((*(m.v + j)).v + (*(m.v + j)).n - 1));
+    }
+  printf ("end]\n");
+
+}
+
+void
+setDebug (char *arg)
+{
+
+  d1, d2, d3 = 0;
+  switch (atoi (arg))
+    {
+    case 3:
+      d3 = 1;
+    case 2:
+      d2 = 1;
+    case 1:
+      d1 = 1;
+    default:
+      break;
+    }
+}
+
+int
+main (int argc, char *argv[])
+{
+  if (argc == 3)
+    setDebug (argv[2]);
+
+  Matrix m = buildMatrix (argv[1]);
+
+  printMatrix (m);
+
+  return 0;
 
 }
